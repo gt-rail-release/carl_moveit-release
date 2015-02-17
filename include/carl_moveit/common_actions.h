b6a6e3797f8cf44d5d259ba8f240a4a447694a52
Russell Toris <rctoris@wpi.edu>
2015-02-17 14:54:48 -0500

    Imported upstream version '0.0.7' of 'upstream'

--- include/carl_moveit/common_actions.h
+++ include/carl_moveit/common_actions.h
@@ -5,6 +5,8 @@
 #include <actionlib/server/simple_action_server.h>
 #include <actionlib/client/simple_action_client.h>
 #include <carl_moveit/MoveToJointPoseAction.h>
+#include <std_srvs/Empty.h>
+#include <wpi_jaco_msgs/GetAngularPosition.h>
 #include <wpi_jaco_msgs/HomeArmAction.h>
 
 #define NUM_JACO_JOINTS 6
@@ -14,17 +16,31 @@ class CommonActions
 
 public:
 
+  /**
+  * \brief Constructor
+  */
   CommonActions();
 
 private:
   ros::NodeHandle n;
   ros::Publisher angularCmdPublisher;
 
+  ros::ServiceClient eraseTrajectoriesClient;
+  ros::ServiceClient jacoPosClient;
+
   actionlib::SimpleActionClient<carl_moveit::MoveToJointPoseAction> moveToJointPoseClient;
   actionlib::SimpleActionServer<wpi_jaco_msgs::HomeArmAction> readyArmServer;
 
   std::vector<float> homePosition;
 
+  /**
+  * \brief Move arm to the home position with obstacle avoidance
+  *
+  * Action server to move the arm to the ready (JACO home) position, optionally followed by a move to a retracted
+  * position.
+  *
+  * @param goal Ready/retract action goal.
+  */
   void readyArm(const wpi_jaco_msgs::HomeArmGoalConstPtr &goal);
 };
 
